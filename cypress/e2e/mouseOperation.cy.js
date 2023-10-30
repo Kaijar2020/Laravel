64248acf5f9cba2fc5c7372f8c1c60233a5581de
Kaijar2020 <kizar.cse@gmail.com>
2023-10-30 21:49:15 +0600

    scroll done

--- cypress/e2e/mouseOperation.cy.js
+++ cypress/e2e/mouseOperation.cy.js
@@ -1,3 +1,5 @@
+require ("@4tw/cypress-drag-drop") //for using drag-drop operation plugin declaration needeed.
+
 describe('Mouse Operation', ()=>{
 
     //Mouse hover operation
@@ -27,9 +29,41 @@ describe('Mouse Operation', ()=>{
 
     //To performe Double click from mouse
     it.skip('Double-click',()=>{
-        // cy.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_ondblclick_html')
+        // cy.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_ondblclick_addeventlistener')
+        // cy.frameloaded('#iframeResult')
         // cy.get('#demo').trigger('dblclick')
         // cy.wait(1000)
         // cy.get('#demo').should('have.text', 'I was double-clicked!')
+    })
+
+    //Perform Drag-Drop operation    
+    it('Drag-Drope perfofrme',()=>{
+        cy.visit('http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+        cy.wait(3000)
+        cy.get('#box6').drag('#box106',{force:true})
+        /** 
+         To use drag function just have to install plugin using this command--
+         $ npm install --save-dev @4tw/cypress-drag-drop
+        */
+
+    })
+
+    //Scrolin the Page 
+    it.only('Scroll the page',()=>{
+
+        //Scroll down
+        cy.visit('https://www.countries-ofthe-world.com/flags-of-the-world.html')
+        cy.wait(1000)
+        cy.get(':nth-child(2) > tbody > :nth-child(97) > :nth-child(2)').scrollIntoView({duration:2000})
+        cy.get(':nth-child(2) > tbody > :nth-child(97) > :nth-child(2)').should('be.visible')
+
+        //scroll Up
+        cy.wait(1000)
+        cy.get(':nth-child(1) > tbody > :nth-child(16) > :nth-child(1) > img').scrollIntoView({duration:3000})
+        cy.get(':nth-child(1) > tbody > :nth-child(16) > :nth-child(1) > img').should('be.visible')
+        cy.wait(1000)
+        //scroll till footer
+        cy.get('#footer').scrollIntoView({duration:4000})
+        cy.get('#footer > div').should('be.visible')
     })
 })
